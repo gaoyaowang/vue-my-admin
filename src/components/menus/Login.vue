b0e387cf98bc7a13cfc3f950670c17de5dbeb199
高耀旺 <gao.yaowang@chinaott.onaliyun.com>
2019-12-11 16:49:26 +0800

    input清除记忆

--- src/components/menus/Login.vue
+++ src/components/menus/Login.vue
@@ -19,7 +19,7 @@
         </svg>
         <div class="form">
           <label for="userName">账号</label>
-          <input type="text" id="userName" v-model="form.userName" @focus="userNameFocus()">
+          <input type="text" id="userName" v-model="form.userName" autocomplete="off" @focus="userNameFocus()">
           <label for="password">密码</label>
           <input type="password" id="password" v-model="form.password" @focus="passwordFocus()">
           <input type="submit" id="submit" value="登 录" @click="submitFocus()">
